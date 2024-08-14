2bbad4826e6349328e74b24a7d7fa8a996abf0d1
unknown <shasmina6103@gmail.com>
2024-08-14 22:06:24 +0530

    Added async.js

---
+++ async.js
@@ -0,0 +1,19 @@
+function async(message, delay) {
+    return new Promise((resolve) => {
+        resolve(message);
+      }, delay);
+  }
+  async function task() {
+    try {
+      console.log('Start Async Operation');
+      const result1 = await async('First operation completed');
+      console.log(result1);
+      const result2 = await async('Second operation completed');
+      console.log(result2);
+      console.log('All operations completed.');
+    } catch (error) {
+      console.error('An error occurred:', error);
+    }
+  }
+    task();
+  
